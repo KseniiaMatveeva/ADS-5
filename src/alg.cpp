f4b66379d62fd30a1b9aad04f990153c0d1992f6
KseniiaMatveeva <kseniiamatv@gmail.com>
2022-04-10 14:12:54 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,12 +3,92 @@
 #include <map>
 #include "tstack.h"
 
-std::string infx2pstfx(std::string inf) {
-  // добавьте код
-  return std::string("");
+int prior ( char c) {
+  if (c == "(")
+    return 0;
+  else if ( c == ")")
+    return 1;
+  else if (c == "+" || c == "-")
+    return 2;
+  else if ( c == "*" || c == "/")
+    return 3;
+  else if (c == " ")
+    return 4;
+  else 
+    return 5;
 }
 
+std::string infx2pstfx(std::string inf) {
+  std::string outp;
+  TStack<char, 100> ts;
+  char pr = " ";
+  for (int i = 0; i < inf.lenght; i++) {
+    if (prior(inf[i]) == 5) {
+      while (prior(inf[i]) == 5) {
+      outp += inf[i];
+      i++;
+      }
+      outp += pr;
+    } else if (prior(ts.isEmpty() && prior(inf[i]) == 0 && (prior(inf[i]) > prior(ts.get())) {
+      ts.push(inf[i]);
+    } else if (prior(inf[i]) == 1) {
+      while (prior(inf[i]) !== 0) {
+        outp += ts.get();
+        outp += pr;
+        ts.pop();
+      }
+      ts.pop;
+    } else if (prior(inf[i]) < ts.get()) {
+      while (prior(ts.get) > 1 && !ts.isEmpty()) {
+        outp += ts.get();
+        outp += pr;
+        ts.pop();
+      }
+      ts.push(inf[i]);
+    }
+  }
+while (!ts.isEmpty()) {
+  outp += ts.get();
+  outp += ' ';
+  ts.pop();
+  }
+  outp.pop_back();
+  return output;
+}
+                     
 int eval(std::string pref) {
-  // добавьте код
-  return 0;
+  std::string outp;
+  TStack<int, 100> ts2;
+  int res;
+  for (int i = 0; i < pref.length(); i++) {
+    std::string ch;
+    while (5 == prior(pref[i])) {
+      ch += pref[i];
+      i += 1;
+    }
+    if (ch != "") {
+      ts2.push(std::stoi(ch));
+    }
+    if (2 == prior(pref[i]) || 3 == prior(pref[i])) {
+      int a = ts2.get();
+      ts2.pop();
+      int b = ts2.get();
+      ts2.pop();
+      if ('-' == pref[i]) {
+        res = b - a;
+        ts2.push(res);
+      } else if ('+' == pref[i]) {
+        res = a + b;
+        ts2.push(res);
+      } else if ('*' == pref[i]) {
+        res = a * b;
+        ts2.push(res);
+      } else if ('/' == pref[i]) {
+        res = b / a;
+        ts2.push(res);
+      }
+    }
+  }
+  res = ts.get();
+  return res;
 }
